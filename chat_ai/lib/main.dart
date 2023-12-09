aa0fe9a6dbc2a7c03cc407719a57507967b2d97a
JojoDom <adwoa_dadson@yahoo.com>
2023-12-09 05:58:25 +0000

    added firebase packaged and google sign in package

--- chat_ai/lib/main.dart
+++ chat_ai/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
+  
   await dotenv.load(fileName: "assets/.env");
   runApp(const MyApp());
 }
