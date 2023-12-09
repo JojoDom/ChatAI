3653cec1de856a098851c9035cbe90ebb422b89a
JojoDom <adwoa_dadson@yahoo.com>
2023-12-09 14:16:19 +0000

    clean up

--- chat_ai/lib/screens/welcome_page.dart
+++ chat_ai/lib/screens/welcome_page.dart
@@ -1,6 +1,5 @@
 import 'package:chat_ai/screens/chat_screen.dart';
 import 'package:chat_ai/widgets/custom_button.dart';
-import 'package:chat_ai/widgets/image_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
